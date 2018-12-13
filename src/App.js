29c639a11dcf9ce07856f911ad108cb1592f98ea
Carol Enache <enache.carol@gmail.com>
2018-12-13 23:54:04 +0000

    create the form slider component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import StepList from './component/step-list';
+import FormSlider from './component/form-slider';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <StepList />
+      <FormSlider sliderWidth="600px" />
     </div>
   );
 }
